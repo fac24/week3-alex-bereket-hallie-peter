acd7c67dd1169b99f19bd2a4475e24fb3d89e05c
PJSalter <45575016+PJSalter@users.noreply.github.com>
2022-05-25 17:25:50 +0100

    modified with email

--- routes/sign-up.js
+++ routes/sign-up.js
@@ -9,7 +9,7 @@ const get = (req, res) => {
     <form method="POST">
     <label for="username">Your username</label>
     <input id="username"></input>
-    <label for="email">Email please</label>
+    <label for="email">Email please"</label>
     <input id="email"></input>
     <label for="password">Your password</label>
     <input id="password" type="password"></input>
