4c693cae2fda337c16227c30fa3de47686cbe455
PJSalter <45575016+PJSalter@users.noreply.github.com>
2022-05-25 16:45:15 +0100

    added email in form

--- routes/sign-up.js
+++ routes/sign-up.js
@@ -1,15 +1,22 @@
 const layout = require("../layout");
 
 const get = (req, res) => {
-    res.send(layout(`sign-up`, `
+  res.send(
+    layout(
+      `sign-up`,
+      `
     <h1>Sign Up</h1>
     <form method="POST">
     <label for="username">Your username</label>
-    <input id="name"></input>
+    <input id="username"></input>
+    <label for="email">Email please</label>
+    <input id="email"></input>
     <label for="password">Your password</label>
     <input id="password" type="password"></input>
     <button type="submit">Submit</button>
-    </form>`));
-}
+    </form>`
+    )
+  );
+};
 
 module.exports = { get };
