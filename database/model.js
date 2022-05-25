7dda3fe2d2f4c82218e1757a9e67a4cf1f38df76
bereket <mebrahtubereket@gmail.com>
2022-05-25 18:01:19 +0100

    removed cypres integration files and added test.js

---
+++ database/model.js
@@ -0,0 +1,32 @@
+const db = require("./connection.js");
+
+function createUser(username, email, password) {
+  const INSERT_USER = `
+    INSERT INTO users (email, password, name) VALUES ($1, $2, $3)
+    RETURNING id, email, name
+  `;
+  return db
+    .query(INSERT_USER, [username, email, password])
+    .then((result) => result.rows[0]);
+}
+
+function getUser(email) {
+  const SELECT_USER = `
+    SELECT id, email, password, name FROM users WHERE email=$1
+  `;
+  return db.query(SELECT_USER, [email]).then((result) => result.rows[0]);
+}
+
+function getSession(sid) {
+  const SELECT_SESSION = "SELECT data FROM sessions WHERE sid=$1";
+  return db.query(SELECT_SESSION, [sid]).then((result) => {
+    const singleResult = result.rows[0];
+    return singleResult && singleResult.data;
+  });
+}
+
+module.exports = {
+  createUser,
+  getUser,
+  getSession,
+};
