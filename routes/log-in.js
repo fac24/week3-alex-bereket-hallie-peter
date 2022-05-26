0f38dd81a18b1a8beabb869f2eeb11991d27a86d
PJSalter <45575016+PJSalter@users.noreply.github.com>
2022-05-26 15:58:59 +0100

    created log in functionality

--- routes/log-in.js
+++ routes/log-in.js
@@ -1,7 +1,12 @@
+const bcrypt = require("bcryptjs/dist/bcrypt");
 const layout = require("../layout");
+const model = require("../database/model");
 
 const get = (req, res) => {
-    res.send(layout(`log-in`, `
+  res.send(
+    layout(
+      `log-in`,
+      `
     <h1>Log In</h1>
     <form method="POST">
     <label for="username">Your username</label>
@@ -9,7 +14,32 @@ const get = (req, res) => {
     <label for="password">Your password</label>
     <input id="password" type="password"></input>
     <button type="submit">Submit</button>
-    </form>`));
+    </form>`
+    )
+  );
+};
+
+function verifyUser(email, password) {
+  return model.getUser(email).then((user) => {
+    return bcrypt.compare(password, user.password).then((match) => {
+      if (!match) {
+        throw new Error("password mismatch");
+      } else {
+        return user;
+      }
+    });
+  });
 }
 
-module.exports = { get };
+function post(req, res) {
+  const { email, password } = req.body;
+  auth
+    .verifyUser(email, password)
+    .then(auth.saveUserSession)
+    .then((sid) => {
+      res.cookie("sid", sid, auth.COOKIE_OPTIONS);
+      res.redirect("/all-posts");
+    });
+}
+
+module.exports = { get, post };
