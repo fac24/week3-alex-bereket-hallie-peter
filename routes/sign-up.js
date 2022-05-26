bd76224a7efa44daca1ec41c6ed584ead602ccbc
vasystus <artgalla8@gmail.com>
2022-05-26 18:06:18 +0100

    requirements added to the form

--- routes/sign-up.js
+++ routes/sign-up.js
@@ -9,13 +9,39 @@ const get = (request, response) => {
       `
     <h1>Sign Up</h1>
     <form method="POST">
-    <label for="username">Your username</label>
-    <input id="username"></input>
-    <label for="email">Email please"</label>
-    <input id="email"></input>
-    <label for="password">Your password</label>
-    <input id="password" type="password"></input>
-    <button type="submit">Submit</button>
+
+    <label for="username">Enter your username</label>
+    <input id="username"></input><br><br>
+
+    <label for="email">Email
+        <span style="color:#ff0000" aria-hidden="true">*</span> 
+    </label>
+    <input type="email" 
+        id="email"
+        aria-invalid="false"
+        aria-describedby="emailError" required/><br><br>
+
+
+    <label for="password">Password
+        <span style="color:#ff0000" aria-hidden="true">*</span>
+    </label>
+    <div id="passwordRequirements">
+        <p>Password must contain the following:</p>
+          <ul>
+            <li>A lowercase letter</li>
+            <li>A capital (uppercase) letter</li>
+            <li>A number</li>
+            <li>Minimum 8 characters</li>
+          </ul>
+    </div>
+     <input type="password"
+          id="password"
+          aria-invalid="false"
+          aria-describedby="passwordRequirements passwordError"
+          pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+          title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" required/>
+    
+          <button type="submit">Submit</button>
     </form>`
   )
   );
